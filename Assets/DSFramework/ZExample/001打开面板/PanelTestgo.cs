6fa81fdd58d4ee0c1684d4456c3de67b8c6a0a5b
UnitySir <zoujack@foxmail.com>
2020-09-03 20:26:50 +0800

    新增简易缓存池

---
+++ Assets/DSFramework/ZExample/001打开面板/PanelTestgo.cs
@@ -0,0 +1,36 @@
+﻿/****************************************************
+--------------------------------
+    ----------------------------
+    文件名称：
+    作者：邹建
+    创建日期：2020年09月03日 17:31:25
+    ----------------------------
+    ----------------------------
+    修改次数：0
+    修改人员：
+    修改日期：
+    ----------------------------
+    ----------------------------
+    功能描述：
+    ----------------------------
+--------------------------------
+*****************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTestgo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PanelMgr.Instance.Open<PanelTest>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/DSFramework/ZExample/002缓存池测试/DelayPush.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayPush : MonoBehaviour
+{
+    private void OnEnable() {
+        Invoke("Push", 2f);
+    }
+
+    private void Push() {
+        PoolMgr.Instance.PushObj("Example/002缓存池/Cube" , gameObject);
+    }
+
+}
